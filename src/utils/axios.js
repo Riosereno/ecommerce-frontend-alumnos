5fda4a232fa85417b1517aa606b5f989b7f53ee8
Riosereno <99235810+Riosereno@users.noreply.github.com>
2023-06-09 11:10:25 -0500

    Update axios.js
    cambio de url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import deafultAxios from 'axios';
 
 const axios = () => deafultAxios.create({
-    baseURL: 'https://e-commerce-api-v2.academlo.tech/api/v1',
+    baseURL: 'https://proyecto-final-mogk.onrender.com',
     headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`
     }
